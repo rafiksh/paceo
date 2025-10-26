be478bb6955039ba6a3e940776799981313d549b
Rafik <rafikshmoury@gmail.com>
2025-10-26 20:15:22 +0100

    refactor: remove old builder tab and related files
    - Remove builder tab layout and screens
    - Remove ActivitySelectionScreen (replaced by new home flow)
    - Clean up unused navigation structure

--- src/app/(tabs)/builder/configure.tsx
+++
@@ -1,5 +0,0 @@
-import { WorkoutConfigurationScreen } from "@/screens/WorkoutConfigurationScreen"
-
-export default function WorkoutConfigureScreen() {
-  return <WorkoutConfigurationScreen />
-}
--- src/app/(tabs)/builder/index.tsx
+++
@@ -1,5 +0,0 @@
-import { ActivitySelectionScreen } from "@/screens/ActivitySelectionScreen"
-
-export default function BuilderIndexScreen() {
-  return <ActivitySelectionScreen />
-}
